7c1a768d8bd212ee3cfe82dc479c9a3c0cba5c64
leo-alcantara <leoalcantara@icloud.com>
2021-07-06 08:50:00 +0200

    1st commit Tuesday. (Fields and constructors. Methods implementation almost complete)

---
+++ course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/data/dao/StudentCollectionRepository.java
@@ -0,0 +1,70 @@
+package se.lexicon.course_manager_assignment.data.dao;
+
+
+
+import se.lexicon.course_manager_assignment.model.Student;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+
+public class StudentCollectionRepository implements StudentDao {
+
+    private Collection<Student> students;
+    Student student;
+
+    public StudentCollectionRepository(Collection<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public Student createStudent(String name, String email, String address) {
+        Student student = new Student(name,email,address);
+        return null;
+    }
+
+    @Override
+    public Student findByEmailIgnoreCase(String email) {
+        for(Student student:students){
+            if(student.getEmail().equalsIgnoreCase(email)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Student> findByNameContains(String name) {
+        students.contains(student.getName());
+        return null;
+    }
+
+    @Override
+    public Student findById(int id) {
+        for(Student student:students){
+            if(student.getSTUDENTID()==id){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Student> findAll() {
+        HashSet<Student> allStudents = new HashSet<>();
+        allStudents.addAll(students);
+        return allStudents;
+    }
+
+    @Override
+    public boolean removeStudent(Student student) {
+        students.remove(student);
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        this.students = new HashSet<>();
+    }
+}
---
+++ course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -0,0 +1,96 @@
+package se.lexicon.course_manager_assignment.model;
+
+import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
+
+public class Course {
+
+    Collection<Student> students;
+
+    private final int COURSEID;
+    String courseName;
+    LocalDate startDate;
+    int weekDuration;
+    Student student;
+
+    public Course(String courseName, LocalDate startDate, int weekDuration, Student student) {
+        this.COURSEID = CourseSequencer.nextCourseId();
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.student = student;
+    }
+
+    public int getCOURSEID() {
+        return COURSEID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public boolean enrollStudent(Student student) {
+        students.add(student);
+        return true;
+    }
+
+    public boolean unenrollStudent(Student student) {
+        students.remove(student);
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return COURSEID == course.COURSEID && weekDuration == course.weekDuration && courseName.equals(course.courseName) && startDate.equals(course.startDate) && student.equals(course.student);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(COURSEID, courseName, startDate, weekDuration, student);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "COURSEID=" + COURSEID +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", student=" + student +
+                '}';
+    }
+}
